7e7df0b0b824f28669b1fcc0af1bb021f1688ce7
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-19 11:32:37 +0100

    movement restrictions set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -6,27 +7,22 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 
-start_position = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-segments = []
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-for i in start_position:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(i)
-    segments.append(new_segment)
+
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, 1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 screen.title("Snake 1.0")
 screen.exitonclick()
