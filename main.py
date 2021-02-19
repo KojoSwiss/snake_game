91b44519d9c41a6bedb50ed7ffc9e3e4de9f9179
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-19 10:37:31 +0100

    moving the snake

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ screen.bgcolor("black")
 
 start_position = [(0, 0), (-20, 0), (-40, 0)]
 
+segments = []
+
 for i in start_position:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(i)
+    segments.append(new_segment)
 
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
