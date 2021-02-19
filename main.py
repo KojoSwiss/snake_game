203d09574811ae6da5fc593208885263974f6d96
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-19 10:43:42 +0100

    snake moving in unison

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
 
 start_position = [(0, 0), (-20, 0), (-40, 0)]
 
@@ -15,8 +17,11 @@ for i in start_position:
     new_segment.goto(i)
     segments.append(new_segment)
 
+
 game_is_on = True
 while game_is_on:
+    screen.update()
+    time.sleep(0.1)
     for seg in segments:
         seg.forward(20)
 
