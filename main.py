1be5f0f3d63c051e7e0dc370a71f9cdc1bcbb5e8
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-19 10:31:44 +0100

    initial snake body created

--- main.py
+++ main.py
@@ -3,5 +3,19 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+
+start_position = [(0, 0), (-20, 0), (-40, 0)]
+
+for i in start_position:
+    new_segment = Turtle()
+    new_segment.color("white")
+    new_segment.goto(i)
+
+
+
+
+
+
+
 screen.title("Snake 1.0")
 screen.exitonclick()
