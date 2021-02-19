0de2a5c97b6d159cf1f561ba8c5bd640f0255fe1
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-19 10:54:33 +0100

    snake moves automatically

--- main.py
+++ main.py
@@ -17,18 +17,16 @@ for i in start_position:
     new_segment.goto(i)
     segments.append(new_segment)
 
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
 
-
-
-
-
+    for seg_num in range(len(segments) - 1, 0, 1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 screen.title("Snake 1.0")
 screen.exitonclick()
