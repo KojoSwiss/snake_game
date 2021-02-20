8df05d9edd4b51925d96c9bdd433287632085e54
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-20 11:41:42 +0100

    Snake able to eat the food and the food regenerates at a random location

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -8,6 +9,7 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -15,14 +17,15 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.title("Snake 1.0")
 screen.exitonclick()
